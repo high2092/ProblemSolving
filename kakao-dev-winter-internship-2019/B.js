7f0304b2083df0afce5148c3a1637c0383179ae6
hhyuw <high2092@gmail.com>
2023-03-30 18:54:32 +0900

    2019 KAKAO DEV WINTER INTERNSHIP - 튜플 (JavaScript, 100.0/100.0)

--- kakao-dev-winter-internship-2019/B.js
+++ kakao-dev-winter-internship-2019/B.js
@@ -1,41 +1,12 @@
-function 집합_정렬(s, idx) { // {1, 3, 2}, {2}, ...
-
-    const sorted = [];
-    while (idx < s.length) {
-        if (s[idx] === '{') {
-            idx++;
-
-            let resultString = '';
-
-            while (s[idx] !== '}') {
-                resultString += s[idx]
-                idx++;
-            }
-
-            const result = resultString.split(',').map(Number);
-            sorted[result.length] = result;
-
-            idx++;
-        }
-
-        idx++;
-    }
-
-
-    return sorted;
-}
+/**
+ * JavaScript 표준 함수를 잘 활용하자
+ */
 
 function solution(s) {
-    var answer = [];
-
-    const sortedSets = 집합_정렬(s, 1);
-
-    for (let i = 1; i < sortedSets.length; i++) {
-        for (const el of sortedSets[i]) {
-            if (answer.includes(el)) continue;
-            answer.push(el);
-        }
-    }
-
-    return answer;
+  return JSON.parse(s.replace(/{/g, '[').replace(/}/g, ']'))
+    .sort((a, b) => a.length - b.length)
+    .reduce((acc, cur) => {
+      acc.push(cur.find((el) => !acc.includes(el)));
+      return acc;
+    }, []);
 }
